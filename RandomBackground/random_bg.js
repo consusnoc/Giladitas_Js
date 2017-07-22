aeb121fbfc1485b0a3c8161431d566825241d848
consusnoc <consueloleusnoc@gmail.com>
2017-07-22 05:06:22 -0300

    Update random bg

--- RandomBackground/random_bg.js
+++ RandomBackground/random_bg.js
@@ -1,11 +1,25 @@
+var boton =document.getElementById("boton_container");
+
+function eventClick (){
+    setInterval(function(){
+    var x= Math.round(Math.random() *255);
+    var y= Math.round(Math.random() *255);
+    var z= Math.round(Math.random() *255);
+    var bg="background:rgb("+x+", "+y+", "+z+"); ";
+    var element=document.getElementById('random_background');
+    element.style=bg;
+    
+    if(boton.onclick = true){
+        clearInterval(element);
+    }
+        
+}, 1500);  
+};
 
 
-setInterval(function(){
-  var x= Math.round(Math.random() *255);
-  var y= Math.round(Math.random() *255);
-  var z= Math.round(Math.random() *255);
-  var bg="background:rgb("+x+", "+y+", "+z+"); ";
-  var element=document.getElementById('random_background');
-  element.style=bg;
-}, 1000);
 
+window.onload = function(){
+    boton.onclick = eventClick;
+    //boton.addEventListener("click", eventoClick);
+    
+}
